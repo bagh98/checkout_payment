d43827f326a2964eb2904ee34f3d90472831b19a
bagh98~ <abonaorz@gmail.com>
2025-02-24 21:56:49 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'features/checkout/presentation/views/my_cart.dart';
+
+void main() {
+  runApp(const CheckoutApp());
+}
+
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyCart(),
+    );
+  }
+}
